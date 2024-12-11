12278e635696e1e377d3ae3e75ac39a1757f612f
ArchTech-Chi <chiomadimgba4@gmail.com>
2024-12-11 19:39:52 +0200

    e-learning

---
+++ Index.js
@@ -0,0 +1,7 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     const joinButton = document.querySelector('.join button');
+//     joinButton.addEventListener('click', () => {
+//       alert('Thank you for joining our community!');
+//     });
+//   });
+  
